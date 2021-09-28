55c401102642eb32e5ce5af617ff2cf66d03e271
vinicius barreto barbosa <vinicius.vbb98@gmail.com>
2021-09-28 17:57:29 -0300

    rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 import {InicioComponent } from './inicio/inicio.component';
 
 
 const routes: Routes = [
 
- {
-   path:"inicio",component:InicioComponent
+  {path:"",redirectTo:'entrar',pathMatch:'full'},
+  {
+    path:"entrar",component:EntrarComponent
+  },
+  {
+    path:"cadastrar",component:CadastrarComponent
+  },
+  {path:"inicio",component:InicioComponent
   }
 ];
 
