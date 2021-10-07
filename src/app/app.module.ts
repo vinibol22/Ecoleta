be4be866467014550d290e8a744b8d881e631406
Maicon <maytech.bonfim@gmail.com>
2021-10-07 09:02:05 -0300

    Implementação da página de suporte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RodapeComponent } from './rodape/rodape.component';
+import { SuporteComponent } from './suporte/suporte.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RodapeComponent
+    SuporteComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
