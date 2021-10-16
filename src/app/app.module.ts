5995a42acacc713dacfad7257108d0cfb52775ce
Maicon <maytech.bonfim@gmail.com>
2021-10-16 15:15:10 -0300

    página de eventos e suporte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SuporteComponent } from './suporte/suporte.component';
+import { EventosComponent } from './eventos/eventos.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SuporteComponent,
+    EventosComponent,
+  
   ],
   imports: [
     BrowserModule,
