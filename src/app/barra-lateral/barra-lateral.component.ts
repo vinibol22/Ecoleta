b3fcb33ecb90af1a8713d58e1fc5551a63f27206
vinicius barreto barbosa <vinicius.vbb98@gmail.com>
2021-10-21 10:17:52 -0300

    barra lateral nova

--- src/app/barra-lateral/barra-lateral.component.ts
+++ src/app/barra-lateral/barra-lateral.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { AlertasService } from '../service/alertas.service';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-barra-lateral',
@@ -6,8 +13,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./barra-lateral.component.css']
 })
 export class BarraLateralComponent implements OnInit {
-
-  constructor() { }
+  postagem: Postagem = new Postagem()
+  foto=environment.foto;
+  constructor(
+    private router: Router,
+    private temaService: TemaService,
+    public auth: AuthService,
+    private postagemService: PostagemService,
+    private alertas: AlertasService
+  ) { }
 
   ngOnInit(){
   }
